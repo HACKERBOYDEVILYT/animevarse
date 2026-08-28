fe3f7f5ffcfec84298507288853c7afaaf14afa7
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 02:12:54 +0600

    Add AnimeGrid component to display anime list

--- src/components/AnimeGrid.jsx
+++ src/components/AnimeGrid.jsx
@@ -1,1 +1,21 @@
+import AnimeCard from "./AnimeCard";
 
+function AnimeGrid({ anime = [] }) {
+  if (!anime.length) {
+    return (
+      <div className="empty-state">
+        <p>No anime found.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="anime-grid">
+      {anime.map((item) => (
+        <AnimeCard key={item.id} anime={item} />
+      ))}
+    </div>
+  );
+}
+
+export default AnimeGrid;
