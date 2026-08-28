495899ac96a04bff67bfd74cd3d9df09325cd1b1
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 02:10:59 +0600

    Add AppRoutes component for application routing

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Search from "../pages/Search";
+import AnimeDetails from "../pages/AnimeDetails";
+import Watch from "../pages/Watch";
+import Trending from "../pages/Trending";
+import Popular from "../pages/Popular";
+import Seasonal from "../pages/Seasonal";
+import Genre from "../pages/Genre";
+import Watchlist from "../pages/Watchlist";
+import History from "../pages/History";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/anime/:id" element={<AnimeDetails />} />
+      <Route path="/watch/:animeId/:episode" element={<Watch />} />
+
+      <Route path="/trending" element={<Trending />} />
+      <Route path="/popular" element={<Popular />} />
+      <Route path="/seasonal" element={<Seasonal />} />
+      <Route path="/genre/:genre" element={<Genre />} />
+
+      <Route path="/watchlist" element={<Watchlist />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/profile" element={<Profile />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
