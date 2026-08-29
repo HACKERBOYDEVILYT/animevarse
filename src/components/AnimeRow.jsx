9dea39631f85d4dffce1f556ad157269afef02fd
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 09:44:00 +0600

    Add AnimeRow component to render AnimeCard items

--- src/components/AnimeRow.jsx
+++ src/components/AnimeRow.jsx
@@ -1,1 +1,11 @@
+import AnimeCard from "./AnimeCard";
 
+export default function AnimeRow({ anime = [] }) {
+  return (
+    <div className="anime-row">
+      {anime.map((item) => (
+        <AnimeCard key={item.id} anime={item} />
+      ))}
+    </div>
+  );
+}
