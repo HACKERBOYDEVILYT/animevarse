ce8bd3bf3f511b772367e630c749575dc25ec11d
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 09:47:20 +0600

    Add GenrePills component for genre navigation

--- src/components/GenrePills.jsx
+++ src/components/GenrePills.jsx
@@ -1,1 +1,27 @@
+import { Link } from "react-router-dom";
 
+const genres = [
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Drama",
+  "Fantasy",
+  "Romance",
+  "School",
+  "Supernatural",
+];
+
+export default function GenrePills() {
+  return (
+    <div className="genre-pills">
+      {genres.map((genre) => (
+        <Link
+          key={genre}
+          to={`/genre/${genre.toLowerCase()}`}
+        >
+          {genre}
+        </Link>
+      ))}
+    </div>
+  );
+}
