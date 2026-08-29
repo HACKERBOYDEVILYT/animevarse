b7d788af9f7ec19745354c3481d8aa082862fc04
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 11:34:57 +0600

    Refactor usePlayerStore to include persistence

--- src/store/useplayerstore.jsx
+++ src/store/useplayerstore.jsx
@@ -1,15 +1,54 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const usePlayerStore = create((set) => ({
-  playing: false,
-  volume: 1,
-  progress: 0,
+const usePlayerStore = create(
+  persist(
+    (set) => ({
+      progress: {},
 
-  play: () => set({ playing: true }),
-  pause: () => set({ playing: false }),
+      saveProgress: (
+        animeId,
+        episode,
+        seconds,
+        duration
+      ) =>
+        set((state) => ({
+          progress: {
+            ...state.progress,
 
-  setVolume: (volume) => set({ volume }),
-  setProgress: (progress) => set({ progress }),
-}));
+            [`${animeId}-${episode}`]: {
+              animeId,
+              episode,
+              seconds,
+              duration,
+              updatedAt: Date.now(),
+            },
+          },
+        })),
+
+      getProgress: (animeId, episode) => {
+        const key = `${animeId}-${episode}`;
+
+        return null;
+      },
+
+      clearProgress: (animeId, episode) =>
+        set((state) => {
+          const copy = { ...state.progress };
+
+          delete copy[
+            `${animeId}-${episode}`
+          ];
+
+          return {
+            progress: copy,
+          };
+        }),
+    }),
+    {
+      name: "animeverse-player",
+    }
+  )
+);
 
 export default usePlayerStore;
