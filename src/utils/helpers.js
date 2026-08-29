b49dbcc0ccdfd8bd3e73f654d501005a71f00002
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 10:01:14 +0600

    Add cn and truncate utility functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,10 @@
+export function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
+export function truncate(text, length = 120) {
+  if (!text) return "";
+  return text.length > length
+    ? `${text.slice(0, length)}...`
+    : text;
+}
