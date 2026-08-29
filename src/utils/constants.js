8fc16cc721eef95db95723a78078ff3203824f88
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-29 10:00:28 +0600

    Add application constants for name, genres, and history

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,14 @@
+export const APP_NAME = "AnimeVerse";
 
+export const GENRES = [
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Drama",
+  "Fantasy",
+  "Romance",
+  "School",
+  "Supernatural",
+];
+
+export const MAX_HISTORY = 50;
